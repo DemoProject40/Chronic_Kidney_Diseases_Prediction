5cee39fa632b11f9bafa30f706700525c23101e6
DemoProject40 <DemoProjecr40@gmail.com>
2023-07-28 18:28:35 +0530

    Pre-processing

---
+++ src/models/data_ingestion.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+# Add the path to the logger file (outer folder) to the Python path
+logging_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+sys.path.append(logging_path)
+
+# Now you can import the logger module from the "src" folder
+from logger import logging
+
+
+def Data_Preprocessing():
+
+    logging.info("Data Ingestion started...")
+
+    #Load the data 
+    df=pd.read_csv(os.path.join('data/raw','kidney_disease.csv'))
+
+    logging.info("Train test split sucessfull..")
+    #Split the data into train and test sets
+    train_data, test_data = train_test_split(df, test_size=0.2, random_state=42)
+
+    #Create the "data_folder" if it doesn't exist
+    data_folder = "data"
+    if not os.path.exists(data_folder):
+        os.makedirs(data_folder)
+
+
+    #Create the "artifacts" folder inside "data_folder" if it doesn't exist
+    artifacts_folder = os.path.join(data_folder, "artifacts")
+    if not os.path.exists(artifacts_folder):
+        os.makedirs(artifacts_folder)
+
+    #Save the train and test data in the "artifacts" folder
+    train_data.to_csv(os.path.join(artifacts_folder, "train_data.csv"), index=False)
+    test_data.to_csv(os.path.join(artifacts_folder, "test_data.csv"), index=False)
+
+    logging.info("Data ingestion Completed...")
+
+    return train_data.shape,test_data.shape
+
+print(Data_Preprocessing())
+
+    
+
+
+    
--- src/models/data_preprocessing.py
+++ src/models/data_preprocessing.py
@@ -3,10 +3,54 @@ import sys
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.impute import SimpleImputer
+from sklearn import preprocessing
 
+# Add the path to the logger file (outer folder) to the Python path
+logging_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+sys.path.append(logging_path)
+
+# Now you can import the logger module from the "src" folder
+from logger import logging
 
 def Data_Preprocessing():
-    df=pd.read_csv(os.path.join('data/raw','kidney_disease.csv'))
-    print(df.head())
+
+    logging.info("Data preprocessing started...")
+
+    #load the train and test data sets
+    df = pd.read_csv(os.path.join('data/raw','kidney_disease.csv'))
+
+    #perform simplerImputer 
+    imp_mode = SimpleImputer(missing_values = np.nan,strategy = 'most_frequent')
+
+    data = pd.DataFrame(imp_mode.fit_transform(df))
+
+    data.columns = df.columns
+    
+    logging.info(data.head())
+
+    # Perform on the train data
+    data['cad'] = data['cad'].apply(lambda x:'no' if x == "\tno" else x )
+
+    data['dm'] = data['dm'].apply(lambda x:'no' if x == "\tno" else x )
+    data['dm'] = data['dm'].apply(lambda x:'yes' if x == "\tyes" else x )
+    data['dm'] = data['dm'].apply(lambda x:'yes' if x == " yes" else x )
+
+    data['rc'] = data['rc'].apply(lambda x:'5.2' if x == "\t?" else x )
+
+    data['wc'] = data['wc'].apply(lambda x:'9800' if x == "\t6200" else x )
+    data['wc'] = data['wc'].apply(lambda x:'9800' if x == "\t8400" else x )
+    data['wc'] = data['wc'].apply(lambda x:'9800' if x == "\t?" else x )
+
+    data['pcv'] = data['pcv'].apply(lambda x:'41' if x == "\t43" else x )
+    data['pcv'] = data['pcv'].apply(lambda x:'41' if x == "\t?" else x )
+
+    data['classification'] = data['classification'].apply(lambda x:'ckd' if x == "ckd\t" else x )
+
+    _df = data.apply(preprocessing.LabelEncoder().fit_transform)
+
+    #_df.to_csv('../data/processed/final_data.csv')
+
+    logging.info("Data preprocessing completed...")
+
+Data_Preprocessing()
